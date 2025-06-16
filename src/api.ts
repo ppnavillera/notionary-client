e44cd3ed86876e9828c065ccfa1ef665d2ae671a
pp.navillera <pp.navillera@gmail.com>
2025-06-16 17:58:13 +0900

    Feat: update API functions to fetch word definitions and save to Notion

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,36 @@
 import axios from "axios";
 
-const url =
-  "https://nxogwwumniauclwarsbt.supabase.co/functions/v1/enkoextension";
+const url = "http://127.0.0.1:54321/functions/v1/";
+// "https://nxogwwumniauclwarsbt.supabase.co/functions/v1/enkoextension";
 
-export async function getWord(word: string) {
+export async function getDefinition(word: string) {
+  const geminiUrl = url + "/gemini";
   try {
     const response = await axios.post(
-      url,
+      geminiUrl,
+      { word: word },
+      {
+        headers: {
+          "Content-Type": "application/json", // JSON 형식 명시
+        },
+      }
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      console.error(err.response?.data || err.message);
+    } else {
+      console.error(err);
+    }
+  }
+}
+
+export async function saveToNotion(word: string) {
+  const notionUrl = url + "/notion";
+  try {
+    const response = await axios.post(
+      notionUrl,
       { word: word }, // "word"로 수정
       {
         headers: {
