32cfa847d43a9e17a6026126d44854e5006f078c
pp.navillera <pp.navillera@gmail.com>
2025-06-16 19:10:50 +0900

    Feat: refactor getDefinition and saveToNotion functions to improve data handling

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Result } from "./interface";
 
 const url = "http://127.0.0.1:54321/functions/v1/";
 // "https://nxogwwumniauclwarsbt.supabase.co/functions/v1/enkoextension";
@@ -6,16 +7,29 @@ const url = "http://127.0.0.1:54321/functions/v1/";
 export async function getDefinition(word: string) {
   const geminiUrl = url + "/gemini";
   try {
-    const response = await axios.post(
-      geminiUrl,
-      { word: word },
-      {
-        headers: {
-          "Content-Type": "application/json", // JSON 형식 명시
+    // const response = await axios.post(
+    //   geminiUrl,
+    //   { word: word },
+    //   {
+    //     headers: {
+    //       "Content-Type": "application/json", // JSON 형식 명시
+    //     },
+    //   }
+    // );
+    // console.log(response.data);
+    const response = {
+      data: {
+        success: true,
+        data: {
+          word: "functions",
+          definition1: "(n) 기능들, 함수들",
+          definition2: "(v) 기능하다, 작동하다",
+          example: "The functions of the brain are complex.",
+          synonyms: ["roles", "purposes", "operations", "tasks", "activities"],
+          antonyms: ["malfunctions"],
         },
-      }
-    );
-    console.log(response.data);
+      },
+    };
     return response.data;
   } catch (err) {
     if (axios.isAxiosError(err)) {
@@ -26,20 +40,22 @@ export async function getDefinition(word: string) {
   }
 }
 
-export async function saveToNotion(word: string) {
+export async function saveToNotion(definition: Result) {
   const notionUrl = url + "/notion";
   try {
     const response = await axios.post(
       notionUrl,
-      { word: word }, // "word"로 수정
+      { data: definition }, // "word"로 수정
       {
         headers: {
           "Content-Type": "application/json", // JSON 형식 명시
         },
       }
     );
-    console.log(response.data);
+
+    // console.log(response.data);
     return response.data;
+    // return;
   } catch (err) {
     if (axios.isAxiosError(err)) {
       console.error(err.response?.data || err.message);
